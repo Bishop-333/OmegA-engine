b80ae20e7c1f45ab88abd5c72008dc3623153ec4
Bishop-333 <99912467+Bishop-333@users.noreply.github.com>
2026-08-31 16:18:22 +0200

    gamma is supported shader side

--- code/sdl/sdl_gamma.c
+++ code/sdl/sdl_gamma.c
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 void GLimp_InitGamma( glconfig_t *config )
 {
-	config->deviceSupportsGamma = qfalse;
+	config->deviceSupportsGamma = qtrue;
 }
 
 
@@ -43,7 +43,7 @@ GLimp_SetGamma
 void GLimp_SetGamma( unsigned char red[256], unsigned char green[256], unsigned char blue[256] )
 {
 	//FIXME Ummmmm... do it with a shader maybe? What about GL1?
-	Com_DPrintf( "Gamma adjustment not supported in SDL3\n" );
+	//Com_DPrintf( "Gamma adjustment not supported in SDL3\n" );
 }
 
 
